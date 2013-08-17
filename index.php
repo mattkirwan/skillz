edf22bf81084bb716f56c26c7c0df5da493a3c4c
Matt Kirwan <matthewkirwan@gmail.com>
2013-08-17 23:15:45 +0100

    Latest dev work

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Skillz</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="css/jumbotron-narrow.css" rel="stylesheet" media="screen">
+    </head>
+    <body>
+        
+
+        <div class="container">
+            <div class="header">
+                <ul class="nav nav-pills pull-right">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Share Your Skill</a></li>
+                    <li><a href="#">Request A Skill</a></li>
+                </ul>
+                
+                <h3 class="text-muted">DevSkillz</h3>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Use your skills, to better your skills.</h1>
+                    <p class="lead">Share your skills. Meet new people. Learn new skills.</p>
+                </div>
+            </div>
+
+            <hr />
+
+            
+            <div class="row">
+                <div class="col-lg-6">
+                    <h4>Help kickstart <i>DevSkillz</i> by offering up your skills:</h4>
+                    <button type="button" class="btn btn-primary btn-lg btn-block">Share your skill</button>
+                </div>
+                <div class="col-lg-6">  
+                    <h4>Want to learn a new skill? Request it here:</h4>              
+                    <button type="button" class="btn btn-primary btn-lg btn-block">Request a skill</button>
+                </div>
+            </div>        
+
+            <hr />
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <h2>Everybody has a skill</h2>
+                    <p><i>DevSkillz</i> allows you to post a skill that you are willing to share, one-to-one.</p>
+                    <p>It might be as simple as an hour of assistance over a pint or as awesome as a day long, in depth look at a specific topic. Are you a PHP master? Javascript demon? Ruby ninja? Whatever your skill if you are willing to share it we want to match it with like-minded developers who need your knowledge.</p>
+                    <p>After submitting your skill, fellow developers will be able to apply to learn your skill. We are essentially acting as a <strong>pimp for your brain</strong>.</p>
+                    <p>There is a catch. If you want to learn a new skill, you have to first successfully share a skill.</p>
+                    <p>When you successfully share a skill you acrue points (1 hour of skill sharing === 1 <i>DevSkillz</i> point) which are then redeemed against learning a new skill.</p>
+                </div>
+            </div>
+
+            <hr />
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="alert alert-success">Have a skill? Share it. Want a new skill? Request it.</div>
+                    <h3>Why?</h3>
+                    <p>It's so easy to get left behind, our industry moves at an incredible pace. Just keeping up with the names of new technologies is hard enough. With this in mind we all know we need to keep learning, naturally we all have different roles, employees and of course personal interests in differing technologies - this diversity is fantastic.</p>
+                    <p>Why can't we use this variety to help each other? An example: Node.js is on my list of things to look at - I don't need it in my day job, but I do want a passing knowledge of it (if only to get by in conversations at the bar during tech conferences)...so why can't we create a central place to request 'An intro to Node.js' - it maybe just be an extended lunch in Newcastle city centre. The developer who shared their skill may have a passing interest in React PHP they could then use their accrued 1 <i>DevSkillz</i> point for this purpose.</p>
+                    <p>The aim isn't for money to change hands, yes I would most likely pay for my Node.js lunch but that isn't the point...ultimately we all have these skills and we all live in our little bubbles...struggling away to learn technology 'x'.</p>
+                    <p>The aim of this site is to collate and assuming we get enough interest I will build out features that allow us to easily find people who need our help and likewise people who can help us.</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <h3>What next?</h3>
+                    <p>I think this idea has the idea to be of use to the wider public (not just dev related topics) - I would for example gladly share my photography skill with somebody and in return go off searching for somebody who can help me learn poker.</p>
+                    <p>For now however (MVP and all that) I think we should stick with developer related subjects...who knows what will come of it.</p>
+                </div>
+            </div>
+          
+            <div class="footer">
+                <p>&copy; Matt Kirwan <?php echo date('Y', time()); ?></p>
+            </div>
+
+        </div>
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="//code.jquery.com/jquery.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+
+    </body>
+</html>
