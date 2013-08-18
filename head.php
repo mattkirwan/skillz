45aed0956ff0916459a425654aa9c55de09cfa18
Matt Kirwan <matthewkirwan@gmail.com>
2013-08-18 13:28:47 +0100

    Added analytics

--- head.php
+++ head.php
@@ -8,6 +8,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
     <link href="css/jumbotron-narrow.css" rel="stylesheet" media="screen">
+    <script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-7628607-3']);
+      _gaq.push(['_setDomainName', 'mattkirwan.com']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>    
     </head>
     <body>
         
